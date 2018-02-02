c3b15f03813eb9c2b63a108da36de9b260f4d23d
Daniel Tavernier <36065155+tabernarious@users.noreply.github.com>
2018-02-02 01:15:49 -0600

    Update f5.automated_backup.v3.1.0.tmpl.tcl

--- f5.automated_backup.v3.1.0.tmpl.tcl
+++ f5.automated_backup.v3.1.0.tmpl.tcl
@@ -1,5 +1,3 @@
-#TMSH-VERSION: 13.1.0.1
-
 # iApp VERSIONS (From what I gathered perusing DevCentral)
 # ~v2.0  - 20140312 - Initially posted releases (v11.4.0-11.6.x? compatibility). (Developed/posted by Thomas Schockaert)
 # v2.1.1 - 20160916 - Retooled SMB upload from smbclient to "mount -t cifs" (v12.1+ compatibility). (Developed/posted by MAG)
